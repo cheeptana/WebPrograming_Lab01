e5f89924dbcf7debcf94545988142578c2ba0d04
cheeptana <163096244+cheeptana@users.noreply.github.com>
2024-08-29 16:17:36 +0700

    Edit

--- app/routes/page.$id.tsx
+++ app/routes/page.$id.tsx
@@ -21,6 +21,7 @@ export default function CardDetail({ }) {
             <center>
                 <div className="my-8">
                     <CardProduct
+                    
                         Id={cardItem.Id}
                         Titla={cardItem.Title}
                         Cover={cardItem.Cover}
